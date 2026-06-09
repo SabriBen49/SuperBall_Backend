65bb26bb7eaf9f4e78d3ee1100f04eed05311456
SabriBen49 <5abrix909@gmail.com>
2026-06-09 10:22:59 +0000

    Add files via upload

---
+++ src/main/java/com/superball/repository/TeamRepository.java
@@ -0,0 +1,14 @@
+package com.superball.repository;
+
+import com.superball.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    Optional<Team> findByName(String name);
+
+    boolean existsByName(String name);
+}
+
---
+++ src/test/java/com/superball/SuperballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
